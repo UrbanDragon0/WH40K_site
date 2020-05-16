9865432da058b919aaeef465209f12a27658f3fa
UrbanDragon0 <64646979+UrbanDragon0@users.noreply.github.com>
2020-05-16 21:14:12 +0300

    Update settings.py

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '75-f*zom#+b5*%7&@p3(1uwi3qpx#da3_)l*0sn)m+ng2*m4bd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
